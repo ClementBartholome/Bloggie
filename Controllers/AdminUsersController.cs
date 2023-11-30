56235e1bce60e04eee7f5f75050cf72de3ae6218
Clément Bartholomé <clementoss@gmail.com>
2023-11-30 19:33:25 +0100

    Feat: Admin can now create and delete users

--- Controllers/AdminUsersController.cs
+++ Controllers/AdminUsersController.cs
@@ -1,6 +1,7 @@
 ﻿using Bloggie.Models.ViewModels;
 using Bloggie.Repositories;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Controllers
@@ -9,12 +10,16 @@ namespace Bloggie.Controllers
     public class AdminUsersController : Controller
     {
         private readonly IUserRepository userRepository;
+        private readonly UserManager<IdentityUser> userManager;
 
-        public AdminUsersController(IUserRepository userRepository)
+        public AdminUsersController(IUserRepository userRepository,
+            UserManager<IdentityUser> userManager)
         {
             this.userRepository = userRepository;
+            this.userManager = userManager;
         }
 
+        [HttpGet]
         public async Task<IActionResult> List()
         {
             var users = await userRepository.GetAll();
@@ -33,6 +38,62 @@ namespace Bloggie.Controllers
             }
 
             return View(usersViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> List(UserViewModel request)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = request.Username,
+                Email = request.Email,
+            };
+
+            var identityResult = await userManager.CreateAsync(identityUser, request.Password);
+
+            if (identityResult is not null)
+            {
+                if (identityResult.Succeeded)
+                {
+                    // assign roles to this user
+                    var roles = new List<string> { "User" };
+
+                    if (request.IsAdmin)
+                    {
+                        roles.Add("Admin");
+                    }
+
+                    identityResult = await userManager.AddToRolesAsync(identityUser, roles);
+
+                    if (identityResult is not null && identityResult.Succeeded)
+                    {
+                        return RedirectToAction("List", "AdminUsers");
+                    }
+
+
+                }
+            }
+
+            return View();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var user = await userManager.FindByIdAsync(id.ToString());
+
+            if (user != null)
+            {
+                   var identityResult = await userManager.DeleteAsync(user);
+
+                if (identityResult is not null && identityResult.Succeeded)
+                {
+                    return RedirectToAction("List", "AdminUsers");
+                }
+            }
+
+            return View();
         }
     }
 }
